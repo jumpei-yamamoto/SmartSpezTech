5e864df0a726138a99429d721df68cbea559a126
jumpei-yamamoto <JumpeiYamamoto@my.uopeople.edu>
2024-09-07 20:02:06 +0900

    ElasticBeanstalkでの実行に80ポートが利用されるよう修正

--- main.py
+++ main.py
@@ -175,4 +175,5 @@ def extract_keywords(answers: List[str]):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
