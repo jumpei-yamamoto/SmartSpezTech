cb769a9d231a120a352c793de050cc861956ec5f
jumpei-yamamoto <JumpeiYamamoto@my.uopeople.edu>
2024-09-08 16:45:22 +0900

    CORS設定の修正

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ app = FastAPI()
 
 # CORSミドルウェアを追加
 origins = [
-    "http://smartspeztech.s3-website-ap-northeast-3.amazonaws.com"
+    "http://smartspeztech.s3-website-ap-northeast-3.amazonaws.com",
+    "https://dov1dxiwhcjvd.cloudfront.net",
 ]
 
 if os.environ.get("ENVIRONMENT") == "development":
